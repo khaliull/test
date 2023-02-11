25d35c85167be43ddbde8caf7d8b7d021e0d2e31
Danis <dan.dan2018@mail.ru>
2023-02-11 18:31:41 +0300

    admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        
+
     ];
 
     /**
@@ -45,4 +45,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAdmin()
+    {
+    return $this->is_admin; 
+    }
 }
